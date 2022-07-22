07799a25d289b7d620f6a20cbbf282366bdd8afc
shakeelask <98741925+shakeelask@users.noreply.github.com>
2022-07-21 22:40:18 -0500

    Update test_mux.py

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -4,19 +4,20 @@ import cocotb
 from cocotb.triggers import Timer
 
 @cocotb.test()
-async def test_mux(dut):
+async def randomised_test_mux(dut):
     """Test for mux2"""
 
-  
-sel = 5'b00010 :out
-out = inp2
-
-#input driving
-dut.sel.value = inp[4:0]
-dut.inp.value = inp[1:0]
-
-await Timer(2, units='ns')
-
-assert dut.out.value = inp[1:0], f"result is incorrect: {dut.X.value} != 2"
-
     cocotb.log.info('##### CTB: Develop your test here ########')
+for i in range(30):
+    SEL = random.randint(0, 4)
+    INP = random.randint(0,30) 
+        
+        dut.sel.value = SEL
+        dut.inp.value = INP
+         await Timer(2, units='ns')
+            
+            dut._log.info(f'INP={INP:30} model={SEL==INP} DUT={int(dut.out.value):30}')
+            
+            assert dut.out.value == INP, "Randomised test failed with: {INP} = {OUT}".format
+            (SEL=dut.sel.value, INP=dut.inp.value, OUT=dut.out.value)
+        
